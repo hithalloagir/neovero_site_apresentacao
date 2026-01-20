9f2f078761aa1c3cf896f800f1d5417a3ee74696
Hithallo Sousa Santos <hsantos.developer@gmail.com>
2026-01-20 15:34:33 -0300

    Feat: Criacao do requirements.txt e ajustes em graficos e criacao dos indicadores

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&fzza)=c*_6tso$u)r2hu7+@q8x$l0ot00_f$b$!58w-3k*riu'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-chave-padrao-caso-falhe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- eng_clinica/urls.py
+++ eng_clinica/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'eng_clinica'
 
 urlpatterns = [
-    path('', views.engenharia_clinica, name='dashboard_eng_clinica'),
+    path('graficos/', views.engenharia_clinica_graficos, name='graficos_eng_clinica'),
+    path('indicadores/', views.engenharia_clinica_indicadores, name='indicadores_eng_clinica'),
 ]
